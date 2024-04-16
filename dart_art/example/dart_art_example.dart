fc964d0d0178f31b838a6bd4044193edd1717994
Dynesshely <catrol@qq.com>
2024-04-16 22:26:38 +0800

    Init default project

---
+++ dart_art/example/dart_art_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_art/dart_art.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ dart_art/test/dart_art_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_art/dart_art.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
