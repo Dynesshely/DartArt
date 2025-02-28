3cde98ec4c436eed3ba3673b1fa186d61761d400
Dynesshely <catrol@qq.com>
2025-02-28 14:21:09 +0800

    Introduce to LCS algorithm

--- dart_art/test/dart_art_test.dart
+++ dart_art/test/dart_art_test.dart
@@ -1,8 +1,9 @@
+import 'package:dart_art/algorithm/text/distance/calculators/lcs.dart';
 import 'package:dart_art/dart_art.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
+  group('A group of tests for units', () {
     test('Text Calculation Test', () {
       var counts = [344, 1458, 16283, 242021, 3423423, 38321892, 342342342, 3423423423];
 
@@ -39,4 +40,36 @@ void main() {
       }
     });
   });
+
+  group('A set of tests for LCS algorithm', () {
+    test('main test', () {
+      var distanceInfo = LCS().getDistanceInfo(["ABCBDAB", "BDCABA"]);
+      expect(4, distanceInfo.distance);
+      var content = '';
+      for (var pair in distanceInfo.lcsInfo!.lcsMatchedSubSequences!.entries) {
+        content += ("\n+ ${pair.key}: ");
+        for (var sequence in pair.value) {
+          content += ("\n    - $sequence");
+        }
+      }
+      print(content);
+      var d1 = <int, List<String>>{
+        4: ["BCBA", "BDAB"]
+      }.assertable();
+      var d2 = distanceInfo.lcsInfo!.lcsMatchedSubSequences!.assertable();
+      print(d1);
+      print(d2);
+      assert(d1 == d2);
+    });
+  });
+}
+
+extension AssertableMapExtensions on Map<int, List<String>> {
+  String assertable() {
+    return entries.map((entry) {
+      var sorted = entry.value;
+      sorted.sort((a, b) => a.compareTo(b));
+      return '${entry.key}: ${sorted.join(',')};';
+    }).join('\n');
+  }
 }
